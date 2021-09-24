3ff61332de9d605201e55899ff94808dd396c9ea
Dave Holst <46893433+daveholst@users.noreply.github.com>
2021-09-24 08:30:24 +0800

    basic bitch counter all in index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import the store
+import { createStore } from 'redux'
+
+
+
+// ACTION - INCREMENT
+const incrememnt = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+const decrememnt = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+// REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+}
+// STORE
+const store = createStore(counter)
+
+// Display in console
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH
+store.dispatch(incrememnt())
+store.dispatch(decrememnt())
+store.dispatch(decrememnt())
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
