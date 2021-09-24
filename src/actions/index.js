09aeaba8130fd5fa4bd054b415ab90d8a9b119ac
Dave Holst <46893433+daveholst@users.noreply.github.com>
2021-09-24 12:10:26 +0800

    end of basic yt tute

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+export const increment = (amount) => {
+  return {
+    type: 'INCREMENT',
+    payload: amount
+  }
+}
+
+export const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
--- src/index.js
+++ src/index.js
@@ -4,46 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import the store
-import { createStore } from 'redux'
+import { createStore} from 'redux'
+import allReducer from './reducers'
+import {Provider} from 'react-redux'
 
-
-
-// ACTION - INCREMENT
-const incrememnt = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
-const decrememnt = () => {
-  return {
-    type: 'DECREMENT'
-  }
-}
-
-// REDUCER
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-  }
-}
-// STORE
-const store = createStore(counter)
-
-// Display in console
-store.subscribe(() => console.log(store.getState()));
-
-// DISPATCH
-store.dispatch(incrememnt())
-store.dispatch(decrememnt())
-store.dispatch(decrememnt())
-
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
